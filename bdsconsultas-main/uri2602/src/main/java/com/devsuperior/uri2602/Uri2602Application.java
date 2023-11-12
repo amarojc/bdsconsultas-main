f4c3390f9367c2f1b5fa4a968221581cff1a44ae
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-11-12 16:30:42 -0300

    uri2602 Run

--- bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,32 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2602Application {
+import com.devsuperior.uri2602.dtos.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
 
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner{
+
+	@Autowired
+	CustomerRepository customerRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<CustomerMinProjection> list = customerRepository.search1("RS");
+		List<CustomerMinDTO> resultDTO1 = list.stream().map(c -> new CustomerMinDTO(c)).collect(Collectors.toList());
+		
+		resultDTO1.stream().map(c -> c).forEach(System.out::println);
 	}
 }
