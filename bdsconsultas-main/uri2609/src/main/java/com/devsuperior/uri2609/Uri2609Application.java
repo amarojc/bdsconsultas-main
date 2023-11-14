2df72c131b32974c45a12278aaff86332bc02fd7
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-11-14 20:56:49 -0300

    uri2609 Run result NativeQuery and JPQL

--- bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,17 +1,22 @@
 package com.devsuperior.uri2609;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import com.devsuperior.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
 
 	@Autowired
-	private CategoryRepository repository;
+	private CategoryRepository categoryRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2609Application.class, args);
@@ -19,6 +24,13 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		System.out.println("\n\n *** RESULT NATIVE QUERY ***");
+		List<CategorySumProjection> list = categoryRepository.seach1();
+		List<CategorySumDTO> result1 = list.stream().map(c -> new CategorySumDTO(c)).collect(Collectors.toList());
+		result1.forEach(System.out::println);
+		
+		System.out.println("\n\n *** RESULT JPQL ***");
+		List<CategorySumDTO> result2 = categoryRepository.seach2();
+		result2.forEach(System.out::println);
 	}
 }
