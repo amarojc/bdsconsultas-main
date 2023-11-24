c3e0e0a5cba1eb30ff2b881ea137f425cc33ddf8
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-11-23 22:42:20 -0300

    uri2737 Run result NativeQuery

--- bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,10 +1,16 @@
 package com.devsuperior.uri2737;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.entities.Lawyer;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
 @SpringBootApplication
@@ -19,7 +25,11 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("\n\n *** RESULT NATIVE QUERY ***");
+		List<LawyerMinProjection> list = repository.search1();
+		List<LawyerMinDTO> resultDTO1 = list.stream().map(x -> new LawyerMinDTO(x)).collect(Collectors.toList());
+		resultDTO1.stream().forEach(System.out::println);
 		
-
+		System.out.println("\n\n *** RESULT JPQL ***");
 	}
 }
