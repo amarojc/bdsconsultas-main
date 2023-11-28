0be3d87d22bef4dc2b7efbbd3252769b86bdf2ec
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-11-27 21:27:47 -0300

    uri2990 Repository

--- bdsconsultas-main/uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
+++ bdsconsultas-main/uri2990/src/main/java/com/devsuperior/uri2990/repositories/EmpregadoRepository.java
@@ -1,9 +1,48 @@
 package com.devsuperior.uri2990.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
 import com.devsuperior.uri2990.entities.Empregado;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, Long> {
 
+	@Query(nativeQuery = true, 
+			value = " SELECT empregados.cpf, empregados.enome, departamentos.dnome "
+					+ "FROM empregados "
+					+ "INNER JOIN departamentos "
+					+ "ON departamentos.dnumero = empregados.dnumero "
+					+ "LEFT JOIN trabalha "
+					+ "ON trabalha.cpf_emp = empregados.cpf "
+					+ "WHERE trabalha.cpf_emp IS NULL "
+					+ "ORDER BY empregados.cpf")
+	List<EmpregadoDeptProjection> search1();
+	
+	@Query(nativeQuery = true, 
+			value = " SELECT empregados.cpf, empregados.enome, departamentos.dnome\n"
+					+ "FROM empregados "
+					+ "INNER JOIN departamentos "
+					+ "ON departamentos.dnumero = empregados.dnumero "
+					+ "WHERE empregados.cpf NOT IN( "
+					+ "	SELECT empregados.cpf "
+					+ "	FROM empregados "
+					+ "	INNER JOIN trabalha "
+					+ "	ON trabalha.cpf_emp = empregados.cpf "
+					+ ") "
+					+ "ORDER BY empregados.cpf")
+	List<EmpregadoDeptProjection> search2();
+	
+	@Query(" SELECT new com.devsuperior.uri2990.dto.EmpregadoDeptDTO(empregado.cpf, empregado.enome, empregado.departamento.dnome) "
+			+ "FROM Empregado empregado "
+			+ "WHERE empregado.cpf NOT IN ( "
+			+ "SELECT empregado.cpf "
+			+ "FROM Empregado empregado "
+			+ "INNER JOIN empregado.projetosOndeTrabalha "
+			+ ") "
+			+ "ORDER BY empregado.cpf")
+	List<EmpregadoDeptDTO> search3();
 }
