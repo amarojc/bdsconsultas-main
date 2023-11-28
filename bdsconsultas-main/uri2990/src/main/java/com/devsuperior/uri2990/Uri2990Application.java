ab48edde456d7b2ab2a952459f482327310b65d6
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-11-27 21:29:56 -0300

    uri2990 Run result NativeQuery LEFT JOIN and NOT IN and JPQL

--- bdsconsultas-main/uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ bdsconsultas-main/uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2990;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 
 @SpringBootApplication
@@ -19,6 +24,18 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("\n\n *** RESULT NATIVE QUERY ***");
+		List<EmpregadoDeptProjection> list1 = repository.search1();
+		List<EmpregadoDeptDTO> result1 = list1.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+		result1.forEach(System.out::println);
 		
+		System.out.println("\n\n *** RESULT NATIVE QUERY NOT IN ***");
+		List<EmpregadoDeptProjection> list2 = repository.search1();
+		List<EmpregadoDeptDTO> result2 = list2.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+		result2.forEach(System.out::println);
+		
+		System.out.println("\n\n *** RESULT JPQL ***");
+		List<EmpregadoDeptDTO> list3 = repository.search3();
+		list3.forEach(System.out::println);
 	}
 }
